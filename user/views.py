8f99fec88a6621dcd3258f0e8384ec11f0c7e550
navnishan <navnishann@gmaol.com>
2026-02-07 21:52:38 +0200

    fix all issues

--- user/views.py
+++ user/views.py
@@ -1,12 +1,12 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics, status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer, AuthTokenSerializer
-from django.contrib.auth import get_user_model
 
 
 class CreateUserView(generics.CreateAPIView):
